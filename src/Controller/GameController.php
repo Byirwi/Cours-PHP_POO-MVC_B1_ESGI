7601c82175f82812db9a4779f759a2aea9c80b89
Byirwi <depaiva.louka@gmail.com>
2025-03-03 14:55:47 +0100

    Add home and combat methods to GameController for rendering game views

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -4,4 +4,14 @@ namespace App\Controller;
 use App\Core\Controller
 use App\Service\CombatService;
 
-class GameController extends Controller {}
+class GameController extends Controller {
+    public function home() {
+        $this->render('home.php', ['title' => 'Bienvenue dans Elden Ring Game']);
+    }
+
+    public function combat() {
+        $combatService = new CombatService();
+        $result = $combatService->startCombat();
+        $this->render('combat.php', ['result' => $result]);
+    }
+}
